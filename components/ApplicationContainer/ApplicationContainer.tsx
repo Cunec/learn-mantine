9a3f66fee879635514dd5521ac838d719e5dde0b
bbs <2xploit@gmail.com>
2022-11-11 15:44:43 +0900

    ApplicationNavbar 분리.

--- components/ApplicationContainer/ApplicationContainer.tsx
+++ components/ApplicationContainer/ApplicationContainer.tsx
@@ -1,25 +1,11 @@
 import { useState } from 'react';
 import {
   AppShell,
-  Navbar,
-  Header,
-  Footer,
-  Aside,
-  Text,
-  MediaQuery,
-  Burger,
   useMantineTheme,
-  Button,
-  NavLink,
-  Modal,
 } from '@mantine/core';
-import { ColorSchemeToggle } from '../ColorSchemeToggle';
-import { IconApple, IconChevronRight, IconCircleOff, IconGauge, IconHome2 } from '@tabler/icons';
-import { AuthenticationForm } from '../AuthenticationForm/AuthenticationForm';
-import Link from 'next/link';
-import HeaderRight from './HeaderRight';
 import ApplicationHeader from './ApplicationHeader';
 import ApplicationFooter from './ApplicationFooter';
+import ApplicationNavbar from './ApplicationNavbar';
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -27,10 +13,10 @@ type LayoutProps = {
 
 export const ApplicationContainer = ({children}: LayoutProps) => {
   const theme = useMantineTheme();
-  const [navbarOpened, setNavbarOpened] = useState(false);
-  
+  const [opened, setOpened] = useState(false);
+
   const openedCallback = (open : boolean) => {
-    setNavbarOpened(open);
+    setOpened(open);
   }
 
   return (
@@ -44,31 +30,7 @@ export const ApplicationContainer = ({children}: LayoutProps) => {
       navbarOffsetBreakpoint="sm"
       asideOffsetBreakpoint="sm"
       navbar={
-        <Navbar p="sm" hiddenBreakpoint="sm" hidden={!navbarOpened} width={{ sm: 200, lg: 300 }}>
-          <NavLink 
-            component={Link}
-            href="/"
-            label="Home"
-            icon={<IconHome2 size={16} stroke={1.5} />} />
-          <NavLink
-            component={Link}
-            href="/board"
-            label="Board"
-            icon={<IconGauge size={16} stroke={1.5} />}
-            rightSection={<IconChevronRight size={12} stroke={1.5} />}
-          />
-          <NavLink 
-            component={Link}
-            href="/about"
-            label="About"
-            icon={<IconApple size={16} stroke={1.5} />} />
-          <NavLink 
-            component={Link}
-            href="/test"
-            label="TestPage"
-            icon={<IconApple size={16} stroke={1.5} />} />
-          <NavLink label="Disabled" icon={<IconCircleOff size={16} stroke={1.5} />} disabled />
-        </Navbar>
+        <ApplicationNavbar opened={opened}/>
       }
       footer={
         <ApplicationFooter />
--- components/ApplicationContainer/ApplicationNavbar.tsx
+++ components/ApplicationContainer/ApplicationNavbar.tsx
@@ -1,5 +1,44 @@
-export default function ApplicationNavbar() {
+import { Navbar, NavLink } from "@mantine/core";
+import { IconApple, IconChevronRight, IconCircleOff, IconGauge, IconHome2 } from "@tabler/icons";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+interface IProps {
+  opened: boolean
+}
+
+export default function ApplicationNavbar({ opened } : IProps) {
+  const [navbarOpened, setNavbarOpened] = useState(false);
+ 
+  useEffect(() => {
+    setNavbarOpened(opened);
+  })
+  
   return (
-    <div></div>
+    <Navbar p="sm" hiddenBreakpoint="sm" hidden={!navbarOpened} width={{ sm: 200, lg: 300 }}>
+      <NavLink 
+        component={Link}
+        href="/"
+        label="Home"
+        icon={<IconHome2 size={16} stroke={1.5} />} />
+      <NavLink
+        component={Link}
+        href="/board"
+        label="Board"
+        icon={<IconGauge size={16} stroke={1.5} />}
+        rightSection={<IconChevronRight size={12} stroke={1.5} />}
+      />
+      <NavLink 
+        component={Link}
+        href="/about"
+        label="About"
+        icon={<IconApple size={16} stroke={1.5} />} />
+      <NavLink 
+        component={Link}
+        href="/test"
+        label="TestPage"
+        icon={<IconApple size={16} stroke={1.5} />} />
+      <NavLink label="Disabled" icon={<IconCircleOff size={16} stroke={1.5} />} disabled />
+    </Navbar>
   )
 }
