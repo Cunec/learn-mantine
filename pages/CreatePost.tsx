b3443ffc6536181a4dc1852affde01020bae80fe
Cunec <2xploit@gmail.com>
2022-11-14 20:51:57 +0900

    ..

---
+++ pages/CreatePost.tsx
@@ -0,0 +1,20 @@
+import { Button, Flex } from "@mantine/core";
+
+export default function CreatePost() {
+  return (
+    <>
+      <Flex
+        mih={50}
+        //bg="rgba(0, 0, 0, .3)"
+        gap="md"
+        justify="flex-start"
+        align="flex-start"
+        direction="column"
+        // mt="-25px"
+        // wrap="wrap"
+      >
+        <Button>aaa</Button>
+      </Flex>
+    </>
+  ) 
+}
